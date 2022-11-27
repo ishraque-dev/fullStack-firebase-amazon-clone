366475e07256f83375c8c4297cd19de9a71fd987
ishraque-dev <ahamedesrak3@gmail.com>
2022-11-27 21:24:08 +0600

    header bar complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build amazon clone!!🔥🔥</h1>
-    </div>
-    // header
-
-    // home
+    <>
+      <Header />
+    </>
   );
 }
 
